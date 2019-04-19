72683c31c96b7199b28c5b64f08488338e19c6f3
HenriqueSabino <h.sabinocosta@gmail.com>
2019-04-19 00:52:09 -0300

    Main view

--- src/application/Main.java
+++ src/application/Main.java
@@ -6,13 +6,23 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
-        primaryStage.setScene(new Scene(root));
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+            Parent parent = loader.load();
+            Scene mainScene = new Scene(parent);
+            primaryStage.setScene(mainScene);
+            primaryStage.setTitle("Sample JavaFX application");
+            primaryStage.show();
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
 

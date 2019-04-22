84153dbc29c9b5b8409bf826222c29500289d205
HenriqueSabino <h.sabinocosta@gmail.com>
2019-04-22 17:29:53 -0300

    SellerForm view (design) and spacing more the error labels

--- src/gui/SellerListController.java
+++ src/gui/SellerListController.java
@@ -1,20 +1,25 @@
 package gui;
 
 import application.Main;
+import gui.util.Alerts;
+import gui.util.Utils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableCell;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.entities.Department;
 import model.entities.Seller;
 import model.services.SellerService;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.Date;
 import java.util.List;
@@ -51,8 +56,9 @@ public class SellerListController implements Initializable {
     private TableColumn<Seller, Department> tableColumnDepartmentName;
     
     @FXML
-    public void onBtNewAction() {
-        System.out.println("onBtNewAction");
+    public void onBtNewAction(ActionEvent event) {
+        Stage stage = Utils.currentStage(event);
+        createDialogForm("/gui/SellerForm.fxml", stage);
     }
     
     public void setSellerService(SellerService service) {
@@ -103,5 +109,25 @@ public class SellerListController implements Initializable {
                 }
             }
         });
+    }
+    
+    private void createDialogForm(String absoluteName, Stage parentStage) {
+        try {
+            
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+            Pane pane = loader.load();
+            
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Enter seller data");
+            
+            dialogStage.setScene(new Scene(pane));
+            dialogStage.setResizable(false);
+            dialogStage.initOwner(parentStage);
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.showAndWait();
+            
+        } catch (IOException e) {
+            Alerts.showAlert("IOException", "Error loading view", e.getMessage(), Alert.AlertType.ERROR);
+        }
     }
 }
